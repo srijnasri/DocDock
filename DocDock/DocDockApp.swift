32dbc178fcd4db9f50338797a5d06b3cd0c8ec98
srijnasri <srijnasri1999@gmail.com>
2024-05-23 21:35:08 +0530

    Initial Commit

---
+++ DocDock/DocDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocDockApp.swift
+//  DocDock
+//
+//  Created by Srijnasri Negi on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DocDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
